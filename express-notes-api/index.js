8bb9bd3e7be48b76601a6746062295f0b5577f0c
Jimmy Vu <JMV3211@gmail.com>
2022-06-28 21:45:10 -0700

    Completed clients can GEt a single note by id portion of express-notes-api

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -13,3 +13,21 @@ app.get('/api/notes', (req, res) => {
   }
   res.status(200).json(dataArray);
 });
+
+app.get('/api/notes/:id', (req, res) => {
+  const id = parseInt(req.params.id, 10);
+  if (Number.isInteger(id) === false || id < 1) {
+    res.status(400).json({ error: "id must be a positive integer" })
+  };
+
+  if (!(Object.keys(data.notes).includes(req.params.id))) {
+    res.status(404).json({ error: `cannot find any notes with the id of ${id}` })
+  }
+
+  for (let note in data.notes) {
+    if (data.notes[note].id === id) {
+      res.status(200).json(data.notes[note]);
+      break;
+    }
+  }
+});
