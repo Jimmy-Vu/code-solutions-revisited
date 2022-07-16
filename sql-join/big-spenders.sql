ef440f0102eb00d731dccba944d556ceea3f19e1
Jimmy Vu <JMV3211@gmail.com>
2022-07-15 21:08:27 -0700

    fixed where are they

--- sql-join/big-spenders.sql
+++ sql-join/big-spenders.sql
@@ -1,1 +1,8 @@
-select *
+select "p"."amount",
+       "c"."firstName" as "First Name",
+       "c"."lastName" as "Last Name",
+       "p"."customerId"
+  from "payments" as "p"
+  join "customers" as "c" using ("customerId")
+  order by "amount" desc
+  limit 10;
---
+++ sql-join/where-are-they.sql
@@ -0,0 +1,7 @@
+select "a"."line1" as "address",
+       "cit"."name" as "city",
+       "a"."district",
+       "cou"."name" as "Country"
+  from "addresses" as "a"
+  join "cities" as "cit" using ("cityId")
+  join "countries" as "cou" using ("countryId");
