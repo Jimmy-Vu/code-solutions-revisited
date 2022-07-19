72ef3c410c3ce683a0849198e65ff4fc8ef0592d
Jimmy Vu <JMV3211@gmail.com>
2022-07-18 19:15:44 -0700

    Completed sql-join exercise

--- sql-join/where-are-they.sql
+++ sql-join/where-are-they.sql
@@ -1,7 +1,7 @@
 select "a"."line1" as "address",
        "cit"."name" as "city",
        "a"."district",
-       "cou"."name" as "Country"
+       "cou"."name" as "country"
   from "addresses" as "a"
   join "cities" as "cit" using ("cityId")
   join "countries" as "cou" using ("countryId");
