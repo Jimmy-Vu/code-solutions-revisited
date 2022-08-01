a66c9c01d1827325e7a211f29ce6795dc6dd8289
Jimmy Vu <JMV3211@gmail.com>
2022-07-31 22:28:56 -0700

    Completed react-elements exercise

--- react-elements/src/index.js
+++ react-elements/src/index.js
@@ -2,6 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const hello = React.createElement('h1', null, 'Hello, React!');
-console.log(hello);
 
-ReactDOM.render(hello);
+ReactDOM.render(hello, document.querySelector('#root'));
