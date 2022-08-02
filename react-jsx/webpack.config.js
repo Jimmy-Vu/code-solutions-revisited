65ddae5bf41d9b82bd939d4ad04cdaaf1b67fdcd
Jimmy Vu <JMV3211@gmail.com>
2022-08-01 23:00:24 -0700

    Completed react-jsx exercise

---
+++ react-jsx/webpack.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            plugins: [
+              '@babel/plugin-transform-react-jsx'
+            ]
+          }
+        }
+      }
+    ]
+  },
+  performance: {
+    hints: false
+  }
+};
