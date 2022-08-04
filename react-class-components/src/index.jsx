b97f75e1df96a41125e6fa9cd8a213fc910ec6f9
Jimmy Vu <JMV3211@gmail.com>
2022-08-03 19:58:36 -0700

    Completed react-class-components exercise with function component

--- react-class-components/src/index.jsx
+++ react-class-components/src/index.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class CustomButton extends React.Component {
-  render() {
-    return <button>{this.props.text}</button>
-  };
+// class CustomButton extends React.Component {
+//   render() {
+//     return <button>{this.props.text}</button>
+//   };
+// }
+
+/* class components are now considered legacy.
+React encourages function components
+so I will attempt to redo this with a function instead */
+
+function CustomButton(props) {
+  return <button>{props.text}</button>;
 }
 
 const element = (
