7750e9deed150c32296ad86b591d683a0e57d9d4
Jimmy Vu <JMV3211@gmail.com>
2022-08-10 18:30:11 -0700

    Set up skeleton of component

---
+++ react-app-drawer-component/src/app-drawer.jsx
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom/client";
+
+
+function AppDrawer(props) {
+  const [isOpened, setIsOpened] = useState(false);
+
+  return (
+    <div className="container">
+
+    </div>
+  );
+}
+
+export default App;
