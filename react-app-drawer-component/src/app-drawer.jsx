e13b88a9eb57ffee2c76df71f691eb82a70119a3
Jimmy Vu <JMV3211@gmail.com>
2022-08-10 19:47:34 -0700

    Completed react-app-drawer-component

--- react-app-drawer-component/src/app-drawer.jsx
+++ react-app-drawer-component/src/app-drawer.jsx
@@ -5,20 +5,47 @@ import ReactDOM from "react-dom/client";
 function AppDrawer(props) {
   const [isOpened, setIsOpened] = useState(false);
 
-  function containerClick() {
+  function backgroundClick() {
     if (isOpened) {
       setIsOpened(false);
     }
   }
   function menuClick() {
-      setIsOpened(true);
+    setIsOpened(true);
   }
 
   return (
-    <div onClick={containerClick} className="container">
-      <i onClick={menuClick} className="fa-solid fa-bars"></i>
+    <div className="container">
+      <section onClick={backgroundClick} className={isOpened ? "background_on" : ""}>
+        <button>
+          <i onClick={menuClick} className="fa-solid fa-bars"></i>
+        </button>
+      </section>
       <section className={`drawer-modal ${isOpened ? "" : "hidden"}`}>
-        <h1>Choose a Game</h1>
+        <nav>
+          <h1>Choose a Game</h1>
+          <ul>
+            <li>
+              <a href="https://www.planetside2.com/home" target="_blank">
+                Planetside 2
+              </a>
+            </li>
+            <li>
+              <a href="https://www.overwatch.com" target="_blank">
+                Overwatch
+              </a>
+            </li>
+            <li>
+              <a href="https://www.maplestory.com" target="_blank">
+                Maple Story
+              </a>
+            </li>
+            <li>
+              <a href="https://www.leagueoflegends.com" target="_blank">
+                League Of Legends
+              </a></li>
+          </ul>
+        </nav>
       </section>
     </div>
   );
