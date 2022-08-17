9302422134a4d3298a916b74336fc832cfc90446
Jimmy Vu <JMV3211@gmail.com>
2022-08-16 18:48:14 -0700

    Completed regex validation challenge for react-validated-input-component

--- react-validated-input-component/src/validated-input.jsx
+++ react-validated-input-component/src/validated-input.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect  } from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
 
 function ValidatedInput(props) {
@@ -6,12 +6,17 @@ function ValidatedInput(props) {
   let mark = '';
   let errorMsg = '';
 
+  const validation = /(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()])/;
+
   if (!input) {
     errorMsg = 'A password is required.';
     mark = 'fa-x';
   } else if (input.length < 8) {
     errorMsg = 'Your password is too short.'
     mark = 'fa-x';
+  } else if (!validation.test(input)) {
+    errorMsg = `Your password requires atleast one capital letter,
+    one digit, and one special character.`;
   } else {
     errorMsg = '';
     mark = 'fa-check';
@@ -21,15 +26,19 @@ function ValidatedInput(props) {
     setInput(e.target.value);
   }
 
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+
   return (
-    <main>
+    <form onSubmit={handleSubmit}>
       <h2>Password</h2>
       <div className="password-row">
-        <input className='password-input' onChange={handleChange} type="text" />
+        <input className='password-input' onChange={handleChange} type="password" />
         <i className={`password-mark fa-solid ${mark}`}></i>
       </div>
       <p className='password-error'>{errorMsg}</p>
-    </main>
+    </form>
   );
 }
 
